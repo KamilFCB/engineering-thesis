0704d2fc9a5b8eca179ec6a675bfefdffb9e4320
KamilFCB <kamil_opole@interia.eu>
2020-04-24 01:54:41 +0200

    added spinner to main page

--- praca_inzynierska/frontend/src/components/layout/MainPage.js
+++ praca_inzynierska/frontend/src/components/layout/MainPage.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Spinner } from "../common/Spinner";
 
 export class MainPage extends Component {
+  static propTypes = {
+    isLoading: PropTypes.bool,
+  };
+
   render() {
-    return <div>Strona główna</div>;
+    const isLoading = this.props.isLoading;
+    const spinner = <Spinner />;
+    const page = <div>Strona główna</div>;
+    return isLoading ? spinner : page;
   }
 }
 
-export default MainPage;
+const mapStateToProps = (state) => ({
+  isLoading: state.auth.isLoading,
+});
+
+export default connect(mapStateToProps)(MainPage);
