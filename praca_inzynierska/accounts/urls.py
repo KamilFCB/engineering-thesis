4e8c88848a4fa7e9a651ee9c188424fba7e7966f
KamilFCB <kamil_opole@interia.eu>
2020-04-24 01:38:12 +0200

    tennis and user profile API url added

--- praca_inzynierska/accounts/urls.py
+++ praca_inzynierska/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .api import RegisterAPI, LoginAPI, UserAPI
+from .api import RegisterAPI, LoginAPI, UserAPI, UserProfileAPI, TennisProfileAPI
 from knox import views as knox_views
 
 
@@ -8,5 +8,7 @@ urlpatterns = [
     path('api/auth/register', RegisterAPI.as_view()),
     path('api/auth/login', LoginAPI.as_view()),
     path('api/auth/user', UserAPI.as_view()),
-    path('api/auth/logout', knox_views.LogoutView.as_view())
+    path('api/auth/logout', knox_views.LogoutView.as_view()),
+    path('api/user/profile', UserProfileAPI.as_view()),
+    path('api/user/tennis_profile', TennisProfileAPI.as_view()),
 ]
