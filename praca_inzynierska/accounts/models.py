fa5f27ad5b75ff93aa9fce7a2c85f6c38970d76e
KamilFCB <kamil_opole@interia.eu>
2020-04-24 01:32:20 +0200

    tennis profile added

--- praca_inzynierska/accounts/models.py
+++ praca_inzynierska/accounts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class TennisProfile(models.Model):
+    LEFT_HANDED = 'LEFT'
+    RIGHT_HANDED = 'RIGHT'
+    DEFAULT = '-'
+    ONE_HANDED = 'ONE'
+    TWO_HANDED = 'TWO'
+
+    FOREHAND_TYPE = [
+        (LEFT_HANDED, 'Leworęczny'),
+        (RIGHT_HANDED, 'Praworęczny'),
+        (DEFAULT, '-')
+    ]
+    BACKHAND_TYPE = [
+        (ONE_HANDED, 'Jednoręczny'),
+        (TWO_HANDED, 'Dwuręczny'),
+        (DEFAULT, '-')
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    residence = models.CharField(max_length=50, blank=True, null=True)
+    birth_date = models.DateField(blank=True, null=True)
+    weight = models.PositiveSmallIntegerField(blank=True, null=True)
+    height = models.PositiveSmallIntegerField(blank=True, null=True)
+    forehand = models.CharField(max_length=10, choices=FOREHAND_TYPE, blank=True, null=True)
+    backhand = models.CharField(max_length=10, choices=BACKHAND_TYPE, blank=True, null=True)
