83f1ec87ad7dbbbab5eabc42b1211a3600992aeb
KamilFCB <kamil_opole@interia.eu>
2020-04-28 21:40:17 +0200

    get tournaments API path set

--- praca_inzynierska/tournaments/urls.py
+++ praca_inzynierska/tournaments/urls.py
@@ -4,4 +4,5 @@ from .api import CreateTournamentAPI, TournamentAPI
 
 urlpatterns = [
     path('api/tournament/create', CreateTournamentAPI.as_view()),
+    path('api/tournament/get', TournamentAPI.as_view())
 ]
