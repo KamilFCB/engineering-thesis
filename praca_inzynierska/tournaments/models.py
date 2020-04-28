986470c6df39ded0c2a032aab44c5a5d31bedcd4
KamilFCB <kamil_opole@interia.eu>
2020-04-28 21:36:51 +0200

    new model tournament

--- praca_inzynierska/tournaments/models.py
+++ praca_inzynierska/tournaments/models.py
@@ -1,1 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
 # Create your models here.
+class Tournament(models.Model):
+    name = models.CharField(unique=True, max_length=100)
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    date = models.DateField()
+    draw_size = models.PositiveSmallIntegerField()
+    description = models.TextField()
+    organizer = models.ForeignKey(User, on_delete=models.CASCADE)
