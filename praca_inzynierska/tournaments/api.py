a475586ce32e82c2077d9435cfbad026c91157f8
KamilFCB <kamil_opole@interia.eu>
2020-05-05 22:47:06 +0200

    incoming and history tournaments API added

--- praca_inzynierska/tournaments/api.py
+++ praca_inzynierska/tournaments/api.py
@@ -1,7 +1,9 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
-from .serializers import TournamentSerializer
-from .models import Tournament
+from .serializers import TournamentSerializer, TournamentsPageSerializer
+from .models import Tournament, Participation
+from django.core.paginator import Paginator
+from django.utils.datetime_safe import datetime
 
 
 class CreateTournamentAPI(generics.GenericAPIView):
@@ -29,3 +31,57 @@ class TournamentAPI(generics.ListAPIView):
     def get_object(self):
         serializer = self.get_serializer(self.queryset)
         return serializer.data
+
+
+class IncomingTournamentsPageAPI(generics.ListAPIView):
+    serializer_class = TournamentsPageSerializer
+    queryset = (Tournament.objects
+                          .filter(date__gt=datetime.now()).order_by('date'))
+
+    def get(self, request, *args, **kwargs):
+        paginator = Paginator(self.queryset, 25)
+        page_number = kwargs['page_number']
+        page_obj = paginator.get_page(page_number)
+        serializer_list = [self.get_serializer(tournament).data
+                           for tournament in page_obj.object_list]
+        if self.request.user.is_authenticated:
+            for tournament in serializer_list:
+                try:
+                    Participation.objects.get(player=self.request.user,
+                                              tournament=tournament['id'])
+                    tournament['participate'] = True
+                except Participation.DoesNotExist:
+                    pass
+        return Response({
+            "tournaments": serializer_list,
+            "hasMore": page_obj.has_next(),
+            'nextPage': page_number+1
+        })
+
+
+class HistoryTournamentsPageAPI(generics.ListAPIView):
+    serializer_class = TournamentsPageSerializer
+    queryset = (Tournament.objects
+                          .filter(date__lt=datetime.now()).order_by('date'))
+
+    def get(self, request, *args, **kwargs):
+        paginator = Paginator(self.queryset, 25)
+        page_number = kwargs['page_number']
+        page_obj = paginator.get_page(page_number)
+        serializer_list = [self.get_serializer(tournament).data
+                           for tournament in page_obj.object_list]
+        if self.request.user.is_authenticated:
+            for tournament in serializer_list:
+                try:
+                    Participation.objects.get(player=self.request.user,
+                                              tournament=tournament['id'])
+                    tournament['participate'] = True
+                except Participation.DoesNotExist:
+                    pass
+        return Response({
+            "tournaments": serializer_list,
+            "hasMore": page_obj.has_next(),
+            'nextPage': page_number+1
+        })
+
+
