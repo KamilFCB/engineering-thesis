eae0fe7a79dce1d7ed6a8a29f0d7a3d0da14c8ce
KamilFCB <kamil_opole@interia.eu>
2020-05-05 22:57:23 +0200

    tournaments reducer created

--- praca_inzynierska/frontend/src/reducers/index.js
+++ praca_inzynierska/frontend/src/reducers/index.js
@@ -3,10 +3,12 @@ import auth from "./auth";
 import errors from "./errors";
 import messages from "./messages";
 import profile from "./profile";
+import tournaments from "./tournaments";
 
 export default combineReducers({
   errors,
   auth,
   messages,
   profile,
+  tournaments,
 });
---
+++ praca_inzynierska/frontend/src/reducers/tournaments.js
@@ -0,0 +1,12 @@
+const initialState = {
+  isLoading: true,
+  incomingTournaments: [],
+  historyTournaments: [],
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
