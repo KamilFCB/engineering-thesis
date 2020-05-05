2f23d97d579a329e2c80b29c278514a6ef152554
KamilFCB <kamil_opole@interia.eu>
2020-05-05 22:48:29 +0200

    tournament participate API and urls added

--- praca_inzynierska/tournaments/api.py
+++ praca_inzynierska/tournaments/api.py
@@ -21,7 +21,7 @@ class CreateTournamentAPI(generics.GenericAPIView):
 
         return Response({
             "message": "Turniej został utworzony"
-        })
+        }, status=201)
 
 
 class TournamentAPI(generics.ListAPIView):
@@ -85,3 +85,57 @@ class HistoryTournamentsPageAPI(generics.ListAPIView):
         })
 
 
+class ParticipateTournamentAPI(generics.RetrieveDestroyAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+    serializer_class = TournamentsPageSerializer
+
+    def post(self, request, *args, **kwargs):
+        tournament = Tournament.objects.get(id=request.data['tournament'])
+        if tournament.date < datetime.now().date():
+            return Response({
+                "message": "Zapisy zostały zakończone"
+            }, status=406)
+
+        participant = (Participation.objects
+                                    .filter(tournament=tournament)
+                                    .count())
+        if participant >= tournament.draw_size:
+            return Response({
+                "message": "Brak wolnych miejsc"
+            }, status=406)
+
+        user = self.request.user
+        participation, created = (Participation
+                                  .objects
+                                  .get_or_create(tournament=tournament,
+                                                 player=user))
+        serializer_data = self.get_serializer(tournament).data
+        serializer_data['participate'] = True
+        return Response({
+            "tournament": serializer_data
+        }, status=201)
+
+    def delete(self, request, *args, **kwargs):
+        try:
+            tournament = Tournament.objects.get(id=request.data['tournament'])
+            user = self.request.user
+            participation = Participation.objects.get(tournament=tournament,
+                                                      player=user)
+            if tournament.date < datetime.now().date():
+                return Response({
+                    "message": "Nie możesz wypisać się z tego turnieju"
+                }, status=406)
+
+            participation.delete()
+            serializer_data = self.get_serializer(tournament).data
+            serializer_data['participate'] = False
+            return Response({
+                "tournament": serializer_data
+            })
+        except Exception:
+            return Response({
+                "message": "Nie byłeś zapisany do tego turnieju"
+            }, status=406)
--- praca_inzynierska/tournaments/urls.py
+++ praca_inzynierska/tournaments/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .api import CreateTournamentAPI, TournamentAPI
+from .api import (CreateTournamentAPI, TournamentAPI,
+                  IncomingTournamentsPageAPI, ParticipateTournamentAPI,
+                  HistoryTournamentsPageAPI)
 
 
 urlpatterns = [
     path('api/tournament/create', CreateTournamentAPI.as_view()),
-    path('api/tournament/get', TournamentAPI.as_view())
+    path('api/tournament/get', TournamentAPI.as_view()),
+    path('api/tournaments/incoming/page/<int:page_number>',
+         IncomingTournamentsPageAPI.as_view()),
+    path('api/tournaments/history/page/<int:page_number>',
+         HistoryTournamentsPageAPI.as_view()),
+    path('api/tournament/participate',
+         ParticipateTournamentAPI.as_view())
 ]
