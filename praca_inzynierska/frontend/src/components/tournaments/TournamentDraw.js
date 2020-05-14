9b74de31badb84a80b65d36a2afc5640e998333c
KamilFCB <kamil_opole@interia.eu>
2020-05-15 00:43:58 +0200

    tournament draw component created

---
+++ praca_inzynierska/frontend/src/components/tournaments/TournamentDraw.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export class TournamentDraw extends Component {
+  render() {
+    return (
+      <div className="tab-pane fade" id="draw">
+      </div>
+    );
+  }
+}
+
+export default TournamentDraw;
