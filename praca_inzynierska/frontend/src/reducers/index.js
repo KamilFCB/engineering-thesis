e69f174a599344629cc14bb33394e7758b7ed9ab
KamilFCB <kamil_opole@interia.eu>
2020-05-15 00:37:22 +0200

    tournament reducer created

--- praca_inzynierska/frontend/src/reducers/index.js
+++ praca_inzynierska/frontend/src/reducers/index.js
@@ -4,6 +4,7 @@ import errors from "./errors";
 import messages from "./messages";
 import profile from "./profile";
 import tournaments from "./tournaments";
+import tournament from "./tournament";
 
 export default combineReducers({
   errors,
@@ -11,4 +12,5 @@ export default combineReducers({
   messages,
   profile,
   tournaments,
+  tournament,
 });
