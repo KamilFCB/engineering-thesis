8e774e84c9f37490f8026afced9c86706db7d15b
KamilFCB <kamil_opole@interia.eu>
2020-07-31 21:20:04 +0200

    site administration added

--- praca_inzynierska/accounts/admin.py
+++ praca_inzynierska/accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import TennisProfile
 
 # Register your models here.
+admin.site.register(TennisProfile)
--- praca_inzynierska/praca_inzynierska/urls.py
+++ praca_inzynierska/praca_inzynierska/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
     path('', include('frontend.urls')),
     path('', include('accounts.urls')),
-    path('', include('tournaments.urls'))
+    path('', include('tournaments.urls')),
+    path('admin/', admin.site.urls)
 ]
